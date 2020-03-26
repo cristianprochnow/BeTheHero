6decd72bb4ef735c8d6f7a434814162956d1f3f0
Cristian Prochnow <48127848+cristianprochnow@users.noreply.github.com>
2020-03-26 16:07:55 -0300

    mobile of the application

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.2.6:3333',
+  baseURL: 'http://(put here the IP address of your PC and remove these brackes):3333',
 });
 
 export default api;
